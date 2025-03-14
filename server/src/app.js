8430aa1b251b3d843b285e4707903514a26abbfc
Avinashkumar3000 <avinashkumarairforce@gmail.com>
2025-03-14 14:58:03 +0530

    feat: update with ejs, static etc...

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// adding middlewares
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
 });
 
 module.exports = app;
